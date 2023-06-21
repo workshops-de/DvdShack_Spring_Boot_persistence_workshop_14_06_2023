767bf170b0958c890a93cba2a4b7e5c0c847e747
Björn Wilmsmann <bjoern@bjoernkw.com>
2023-06-21 13:41:11 +0200

    Sample solution for day 6

--- src/main/resources/db/migration/V2__Create_loyalty_card_table.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE loyalty_card (
-    id INTEGER PRIMARY KEY,
-    points INT NOT NULL DEFAULT 0,
-    customer_id INTEGER NOT NULL,
-    FOREIGN KEY (customer_id) REFERENCES customer(customer_id)
-);
